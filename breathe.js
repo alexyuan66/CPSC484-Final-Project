b723fdd5e6f6906f64d5cd405b0d63733946f5bf
nathalia-reis <nathalia.reis@yale.edu>
2023-04-11 23:53:44 -0400

    Add existing project files prior to the push to GitHub.

---
+++ breathe.js
@@ -0,0 +1,36 @@
+const circle = document.querySelector('.circle');
+let size = 300;
+let intervalId;
+let title = document.querySelector('.title');
+
+//Inhale
+function breathe() {
+  title.textContent = "Inhale";
+  intervalId = setInterval(function() {
+    size += 1;
+    circle.style.width = size + 'px';
+    circle.style.height = size + 'px';
+
+    if (size >= 300) {
+      clearInterval(intervalId);
+      setTimeout(exhale, 2000); // wait for 2 seconds before exhaling
+    }
+  }, 10);
+}
+
+//Exhale
+function exhale() {
+  title.textContent = "Exhale";
+  intervalId = setInterval(function() {
+    size -= 1;
+    circle.style.width = size + 'px';
+    circle.style.height = size + 'px';
+
+    if (size <= 100) {
+      clearInterval(intervalId);
+      setTimeout(breathe, 2000); // wait for 2 seconds before inhaling again
+    }
+  }, 10);
+}
+
+breathe(); // Start the breathing exercise
