ea74fd530e95393a6f98cf884f748db205695e3b
Alex Yuan <alexyuan66@gmail.com>
2023-04-13 14:31:02 -0400

    rebase

--- main-menu.js
+++ main-menu.js
@@ -45,23 +45,19 @@ function tohex(r, g, b) {
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+// Get the health bar element
+const healthBar = document.getElementById("health-bar");
 
-  // Get the health bar element
-  const healthBar = document.getElementById("health-bar");
-
-  // Check if the user has a saved health value in local storage
-  let currentHealth = localStorage.getItem("health");
-  if (currentHealth) {
-    // If a saved value exists, set the health bar to that value
-    healthBar.style.width = `${currentHealth}%`;
-  } else {
-    // Otherwise, start the health bar at 100%
-    healthBar.style.width = "100%";
-    currentHealth = 100;
-  }
-
-})
+// Check if the user has a saved health value in local storage
+let currentHealth = localStorage.getItem("health");
+if (currentHealth) {
+  // If a saved value exists, set the health bar to that value
+  healthBar.style.width = `${currentHealth}%`;
+} else {
+  // Otherwise, start the health bar at 100%
+  healthBar.style.width = "100%";
+  currentHealth = 100;
+}
 
 // Function to update the health bar with a new value
 function updateHealthBar(newHealth) {
@@ -75,11 +71,11 @@ function updateHealthBar(newHealth) {
 // Function to increase the health value
 function increaseHealth() {
   // Increase the health value by 10%
+  currentHealth = parseInt(currentHealth);
   currentHealth += 10;
   if (currentHealth > 100) {
     currentHealth = 100;
   }
-
   // Update the health bar with the new value
   updateHealthBar(currentHealth);
 }
