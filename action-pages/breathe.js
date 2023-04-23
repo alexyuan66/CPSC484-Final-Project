b058b78f65da74002fd8e38222f3cc6c36bca3f7
nathalia-reis <nathalia.reis@yale.edu>
2023-04-23 09:12:36 -0400

    Debugged breathe animation

--- action-pages/breathe.js
+++ action-pages/breathe.js
@@ -1,44 +1,56 @@
-const circle = document.querySelector('.circle');
-let size = 300;
-let intervalId;
-let title = document.querySelector('.title');
-
-//Inhale
-function breathe() {
-  title.textContent = "Inhale";
-  intervalId = setInterval(function() {
-    size += 1;
-    circle.style.width = size + 'px';
-    circle.style.height = size + 'px';
-
-    if (size >= 300) {
-      clearInterval(intervalId);
-      setTimeout(exhale, 2000); // wait for 2 seconds before exhaling
+(function() {
+    const circle = document.querySelector('.circle');
+    const title = document.querySelector('.title');
+    let size = 300;
+  
+    if (!circle || !title) {
+      console.error('Cannot find required element in the DOM');
+      return;
     }
-  }, 10);
-}
-
-//Exhale
-function exhale() {
-  title.textContent = "Exhale";
-  intervalId = setInterval(function() {
-    size -= 1;
-    circle.style.width = size + 'px';
-    circle.style.height = size + 'px';
-
-    if (size <= 100) {
-      clearInterval(intervalId);
-      setTimeout(breathe, 2000); // wait for 2 seconds before inhaling again
+  
+    function breathe() {
+      title.textContent = "Inhale";
+      const inhaleInterval = setInterval(function() {
+        size += 1;
+        circle.style.width = size + 'px';
+        circle.style.height = size + 'px';
+  
+        if (size >= 300) {
+          clearInterval(inhaleInterval);
+          setTimeout(exhale, 2000); // wait for 2 seconds before exhaling
+        }
+      }, 10);
     }
-  }, 10);
-}
-
-breathe(); // Start the breathing exercise
-
-function goToMainMenu() {
-    window.location.href = "../main-menu.html";
-}
-
-function completeBreathing() {
-    window.location.href = "../main-menu.html";
-}
+  
+    function exhale() {
+      title.textContent = "Exhale";
+      const exhaleInterval = setInterval(function() {
+        size -= 1;
+        circle.style.width = size + 'px';
+        circle.style.height = size + 'px';
+  
+        if (size <= 100) {
+          clearInterval(exhaleInterval);
+          setTimeout(breathe, 2000); // wait for 2 seconds before inhaling again
+        }
+      }, 10);
+    }
+  
+    function trackCursor(e) {
+      const cursor = document.querySelector('.cursor');
+      cursor.style.left = e.clientX + 'px';
+      cursor.style.top = e.clientY + 'px';
+    }
+  
+    function goToMainMenu() {
+      window.location.href = "../main-menu.html";
+    }
+  
+    function completeBreathing() {
+      window.location.href = "../main-menu.html";
+    }
+  
+    document.addEventListener('mousemove', trackCursor);
+    breathe();
+  })();
+  t 
