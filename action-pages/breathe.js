9c5724ed9a2208947356c787ae6dd463c1d79441
Alex Yuan <alexyuan66@gmail.com>
2023-04-23 16:12:29 -0400

    fixed health bar

--- action-pages/breathe.js
+++ action-pages/breathe.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let currentHealth = localStorage.getItem("health");
     // Increase the health value by 10%
     currentHealth = parseInt(currentHealth);
-    currentHealth += 10;
+    currentHealth += 5;
     if (currentHealth > 100) {
       currentHealth = 0;
     }
